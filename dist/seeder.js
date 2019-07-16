0dd31afafa989e15efc71e8bf41e5c035bd32709
mastjoe <josephnkwonta@gmail.com>
2019-07-16 23:07:46 +0100

    fix seeder

---
+++ dist/seeder.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var _pg = require("pg");
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var pool = new _pg.Pool();
+
+_dotenv["default"].config();
+
+var user = {
+  first_name: 'admin',
+  last_name: 'admin',
+  email: 'admin@email.com',
+  is_admin: true,
+  password: 'password'
+};
+
+var token = _jsonwebtoken["default"].sign({
+  user: user
+}, process.env.SECRET_KEY, {
+  expiresIn: '2h'
+});
+
+var userSql = "INSERT INTO users\n (first_name, last_name, email, is_admin, password, token, created_at) \n VALUES ('admin', 'admin', 'admin@email.com', 'true','password','".concat(token, "', NOW())");
+pool.query(userSql).then(function (r) {
+  console.log('admin seeded');
+  pool.end();
+})["catch"](function (e) {
+  console.log('admin not seeded' + e);
+  pool.end();
+});
